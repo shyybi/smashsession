8e24fb8581f243ab95641ba07a1cfdcb6aef93ba
GateauLune <146101928+gateaulune@users.noreply.github.com>
2024-07-30 14:17:58 +0200

    Header update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './Assets/moon.png' 
 const moon = require('./Assets/moon.png')
+const searchico = require('./Assets/search-interface-symbol.png')
 function App() {
   return (
     // ne pas utiliser <a></a> pour les paragraphes.
@@ -9,8 +10,13 @@ function App() {
     <div>
       <header id='App-Header' className='flex flex-row justify-around mt-3 '>
         <p className='text-lg mt-2'>SessionSmash</p>
-        <input type='text' placeholder='Recherche par joueurs / régions'  className='bg-gray-300 text-black w-7/12 h-10 rounded-xl pl-2'/>
-
+        <div className='flex flex-row w-7/12 bg-gray-300 rounded-xl pl-2'>
+          <button className='size-8 mt-1 pr-3'>
+            <img src={searchico} alt='moon' />
+          </button>
+          <input type='text' placeholder='Recherche par joueurs / régions'  className='bg-gray-300 text-black rounded-xl h-10 w-full  pl-2 '/>
+        </div>
+        
         <div className='flex flex-row space-x-44 '>
           <img src={moon} alt='moon' className='size-8'/>
           <button className='bg-gray-300 px-7 rounded-xl drop-shadow-lg'>Se Connecter</button>
@@ -28,7 +34,6 @@ function App() {
               <option value='hdr'>Sm4sh</option>
               <option value='brawl'>Smash Brawl</option>
               <option value='melee'>Smash Melee</option>
-
             </select>
             <p>Distance</p>
             <div>
@@ -38,8 +43,11 @@ function App() {
                 <p>100 km</p>
               </div>
             </div>
-            <p>Afficher les sessions pleines</p>
-            <input type='checkbox'></input>
+            <div className='flex flex-row space-x-4'>
+              <p>Afficher les sessions pleines</p>
+              <input type='checkbox'></input>
+            </div>
+            
             <p>Horaire</p>
             <input type='datetime-local'></input>
             <p>Niveau</p>
