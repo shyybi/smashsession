691ae316775531abd401e4bb1c4501779ea3e271
GateauLune <146101928+gateaulune@users.noreply.github.com>
2024-07-30 03:40:31 +0200

    Ajout de Tailwinds CSS et creation de la header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import './Assets/moon.png' 
+const moon = require('./Assets/moon.png')
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header id='App-Header' className='flex flex-row justify-around mt-3 '>
+        <p className='text-lg mt-2'>SessionSmash</p>
+        <input type='text' placeholder='Recherche par joueurs / régions'  className='bg-gray-300 text-black w-7/12 h-10 rounded-xl pl-2'/>
+
+        <div className='flex flex-row space-x-44 '>
+          <img src={moon} alt='moon' className='size-8'/>
+          <button className='bg-gray-300 px-7 rounded-xl drop-shadow-lg'>Se Connecter</button>
+        </div>
       </header>
     </div>
   );
