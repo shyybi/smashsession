b3d8297249ea83054d5e303fc63c4c7327b320f5
GateauLune <146101928+gateaulune@users.noreply.github.com>
2024-07-31 23:41:54 +0200

    Positionnement HomePage

--- src/Home.js
+++ src/Home.js
@@ -39,7 +39,7 @@ function Home({ user, setUser }) {
     return (
         <div>
             <header id='App-Header' className='flex flex-row justify-around mt-3 '>
-                <p className='text-lg mt-2'>SessionSmash</p>
+                <p className='text-lg mt-2'>NaetorUSmash</p>
                 <div className='flex flex-row w-7/12 bg-gray-300 rounded-xl pl-2'>
                     <button type='submit' className='size-8 mt-1 pr-3'>
                         <img src={searchico} alt='moon' />
@@ -62,64 +62,82 @@ function Home({ user, setUser }) {
                     )}
                 </div>
             </header>
-            <div className="m-8 w-1/6 h-screen">
-                <div>
-                    <p>Filtres de recherche</p>
-                    <br />
-                    <div className="flex flex-col gap-8">
-                        <div>
-                            <p>Jeu</p>
-                            <select name='jeu' className="bg-gray-200 rounded-lg p-3 w-full ">
-                                <option value='ultimate'>Smash Ultimate</option>
-                                <option value='hdr'>Smash HDR</option>
-                                <option value='sm4sh'>Sm4sh</option>
-                                <option value='brawl'>Smash Brawl</option>
-                                <option value='melee'>Smash Melee</option>
-                            </select>
-                        </div>
-                        <div>
-                            <div className='flex flex-row space-x-1'>
-                                <p>Distance : </p>
-                                <output>{distance} km</output>
+            <div className="m-8 w-10/12">
+                <div className='flex flox-row'>
+                    <div className='w-72'>
+                        <p>Filtres de recherche</p>
+                        <br />
+                        <div className="flex flex-col gap-8">
+                            <div>
+                                <p>Jeu</p>
+                                <select name='jeu' className="bg-gray-200 rounded-lg p-3 w-full ">
+                                    <option value='ultimate'>Smash Ultimate</option>
+                                    <option value='hdr'>Smash HDR</option>
+                                    <option value='sm4sh'>Sm4sh</option>
+                                    <option value='brawl'>Smash Brawl</option>
+                                    <option value='melee'>Smash Melee</option>
+                                </select>
                             </div>
                             <div>
-                                <input
-                                    type='range'
-                                    min="0"
-                                    max="100"
-                                    value={distance}
-                                    onChange={handleDistanceChange}
-                                    className='w-full'
-                                />
-                                <div className='flex flex-row justify-between'>
-                                    <p>0 km</p>
-                                    <p>100 km</p>
+                                <div className='flex flex-row space-x-1'>
+                                    <p>Distance : </p>
+                                    <output>{distance} km</output>
+                                </div>
+                                <div>
+                                    <input
+                                        type='range'
+                                        min="0"
+                                        max="100"
+                                        value={distance}
+                                        onChange={handleDistanceChange}
+                                        className='w-full'
+                                    />
+                                    <div className='flex flex-row justify-between'>
+                                        <p>0 km</p>
+                                        <p>100 km</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div>
-                            <p>Afficher les sessions pleines</p>
-                            <input type='checkbox'></input>
-                        </div>
-                        <div>
-                            <p>Horaire</p>
-                            <input type='datetime-local' className="bg-gray-200 rounded-lg p-3 w-full "></input>
-                        </div>
-                        <div>
-                            <p>Niveau</p>
-                            <select name='niveau' className="bg-gray-200 rounded-lg p-3 w-full ">
-                                <option value='debutant'>Débutant</option>
-                                <option value='intermediaire'>Intermédiaire</option>
-                                <option value='expert'>Expert</option>
-                            </select>
+                            <div>
+                                <p>Afficher les sessions pleines</p>
+                                <input type='checkbox'></input>
+                            </div>
+                            <div>
+                                <p>Horaire</p>
+                                <input type='datetime-local' className="bg-gray-200 rounded-lg p-3 w-full "></input>
+                            </div>
+                            <div>
+                                <p>Niveau</p>
+                                <select name='niveau' className="bg-gray-200 rounded-lg p-3 w-full ">
+                                    <option value='debutant'>Débutant</option>
+                                    <option value='intermediaire'>Intermédiaire</option>
+                                    <option value='expert'>Expert</option>
+                                </select>
+                            </div>
+                            <div className='flex flex-col justify-center align-bottom h-96 '>
+                                <p>Credits</p>
+                                <p>Coditions d'utilisation</p>
+                                <p>Contacts</p>
+                            </div>
                         </div>
                     </div>
+                    {/* Separation */}
+                    <div className='flex flex-col ml-10'>
+                        <div className='flex flex-row space'>
+                            <p>Sessions proche de chez moi</p>
+                            <button className='bg-gray-200 px-7 rounded-xl drop-shadow-lg'>
+                                Creer une sessions
+                            </button>
+                            <div>
+
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                {/* Separation */}
-                <div>
-                    <p>Sessions proche</p>
-                </div>
+                <p>a </p>
             </div>
+
         </div>
     );
 }
