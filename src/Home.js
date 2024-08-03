18f090a23a2ae98c82f063d7108fcba202e01087
IgenFlo <floehr31@gmail.com>
2024-08-03 23:56:15 +0200

    fix: tooltip + call discord api

--- src/Home.js
+++ src/Home.js
@@ -1,152 +1,177 @@
 import React, { useState, useEffect } from 'react';
-import Tooltip from 'react-tooltip-lite';
-import { Link, useNavigate } from 'react-router-dom';
-import sun from './Assets/sun.svg';
-import moon from './Assets/moon.svg';
-import searchico from './Assets/search-interface-symbol.png';
-import { useTheme } from './common/ThemeContext';
-import './App.css';
+import { Tooltip } from "react-tooltip";
+import { Link, useNavigate } from "react-router-dom";
+import sun from "./Assets/sun.svg";
+import moon from "./Assets/moon.svg";
+import searchico from "./Assets/search-interface-symbol.png";
+import { useTheme } from "./common/ThemeContext";
+import "./App.css";
+import axios from "axios";
 
 function Home({ user, setUser }) {
-    const { theme, toggleTheme } = useTheme();
-    const themeIcon = theme === 'light' ? sun : moon;
-    const [distance, setDistance] = useState(50);
-    const navigate = useNavigate();
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? sun : moon;
+  const [distance, setDistance] = useState(50);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const code = urlParams.get('code');
-        if (code) {
-            fetch(`http://localhost:5000/oauth/callback?code=${code}`)
-                .then(response => response.json())
-                .then(data => {
-                    setUser(data);
-                    localStorage.setItem('user', JSON.stringify(data));
-                    navigate('/');
-                });
-        } else {
-            const storedUser = localStorage.getItem('user');
-            if (storedUser) {
-                setUser(JSON.parse(storedUser));
-            }
-        }
-    }, [navigate, setUser]);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    if (code) {
+      fetch(`http://localhost:5000/discord/callback?code=${code}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setUser(data);
+          localStorage.setItem("user", JSON.stringify(data));
+          navigate("/");
+        });
+    } else {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    }
+  }, [navigate, setUser]);
 
-    const handleLogin = () => {
-        window.location.href = 'http://localhost:5000/oauth/discord';
-    };
+  const handleLogin = async () => {
+    const response = await axios.get("http://localhost:5000/discord");
+    window.location.href = response.data;
+  };
 
-    const handleDistanceChange = (event) => {
-        setDistance(event.target.value);
-    };
+  const handleDistanceChange = (event) => {
+    setDistance(event.target.value);
+  };
 
-    return (
-        <div>
-            <header id='App-Header' className='flex flex-row justify-evenly mt-3'>
-                <p className='text-xl mt-2 ml-8 mr-4'>NaetorUSmash</p>
-                <div className='flex flex-row w-7/12 bg-gray-300 rounded-xl pl-2'>
-                    <button type='submit' className='size-8 mt-1 pr-3'>
-                        <img src={searchico} alt='search icon' />
-                    </button>
-                    <input type='text' placeholder='Recherche par joueurs / régions' className='bg-gray-300 text-black rounded-xl h-10 w-full pl-2' />
-                </div>
-                <button onClick={toggleTheme} className='flex items-center'>
-                    <img src={themeIcon} alt='Theme icon' className='size-8 mt-1 mr-5' />
-                </button>
-                <div className='flex flex-row'>
-                    {user ? (
-                        <Link to="/profile">
-                            <Tooltip content="Accedez à votre profil" direction="down" tagName="span" className="tooltip">
-                                <div className='flex flex-row-reverse'>
-                                    <img src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`} alt='Discord Profile' className='rounded-full w-10 h-10' />
-                                    <span className='mt-2 mr-2'>{user.global_name}</span>
-                                </div>
-                            </Tooltip>
-                        </Link>
-                    ) : (
-                        <button onClick={handleLogin} className='bg-gray-200 px-7 rounded-xl drop-shadow-lg ml-20 transition-all transform hover:bg-gray-300'>
-                            Se Connecter
-                        </button>
-                    )}
-                </div>
-            </header>
-            <div className="m-8 w-10/12">
-                <div className='flex flex-row'>
-                    <div className='w-1/5'>
-                        <p className='text-lg'>Filtres de recherche :</p>
-                        <br />
-                        <div className="flex flex-col gap-8">
-                            <div>
-                                <p>Jeu</p>
-                                <select name='jeu' className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300">
-                                    <option value='ultimate'>Smash Ultimate</option>
-                                    <option value='hdr'>Smash HDR</option>
-                                    <option value='sm4sh'>Sm4sh</option>
-                                    <option value='brawl'>Smash Brawl</option>
-                                    <option value='melee'>Smash Melee</option>
-                                </select>
-                            </div>
-                            <div>
-                                <div className='flex flex-row space-x-1'>
-                                    <p>Distance :</p>
-                                    <output>{distance} km</output>
-                                </div>
-                                <div>
-                                    <input
-                                        type='range'
-                                        min="0"
-                                        max="100"
-                                        value={distance}
-                                        onChange={handleDistanceChange}
-                                        className='w-full'
-                                    />
-                                    <div className='flex flex-row justify-between'>
-                                        <p>0 km</p>
-                                        <p>100 km</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='flex flex-row justify-between'>
-                                <p>Afficher les sessions pleines</p>
-                                <input type='checkbox'></input>
-                            </div>
-                            <div>
-                                <p>Horaire</p>
-                                <input type='datetime-local' className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300"></input>
-                            </div>
-                            <div>
-                                <p>Niveau</p>
-                                <select name='niveau' className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300">
-                                    <option value='debutant'>Débutant</option>
-                                    <option value='intermediaire'>Intermédiaire</option>
-                                    <option value='expert'>Expert</option>
-                                </select>
-                            </div>
-                            <div className='flex flex-col text-lg justify-center align-bottom mt-24 h-auto'>
-                                <p>Credits</p>
-                                <p>Conditions d'utilisation</p>
-                                <p>Contacts</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Separation */}
-                    <div className='flex flex-col ml-10 w-10/12'>
-                        <div className='flex flex-col w-full'>
-                            <div className='flex flex-row justify-between w-full'>
-                                <p className='text-lg mr-10'>Sessions proches de chez moi</p>
-                                <button className='bg-gray-200 px-7 rounded-xl drop-shadow-lg transition-all transform hover:bg-gray-300'>
-                                    <Link to="/create">Créer une session</Link>
-                                </button>
-                            </div>
-                            <div className='mt-14 h-screen'>
-                                <p>aa</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <header id="App-Header" className="flex flex-row justify-evenly mt-3">
+        <p className="text-xl mt-2 ml-8 mr-4">NaetorUSmash</p>
+        <div className="flex flex-row w-7/12 bg-gray-300 rounded-xl pl-2">
+          <button type="submit" className="size-8 mt-1 pr-3">
+            <img src={searchico} alt="search icon" />
+          </button>
+          <input
+            type="text"
+            placeholder="Recherche par joueurs / régions"
+            className="bg-gray-300 text-black rounded-xl h-10 w-full pl-2"
+          />
         </div>
-    );
+        <button onClick={toggleTheme} className="flex items-center">
+          <img src={themeIcon} alt="Theme icon" className="size-8 mt-1 mr-5" />
+        </button>
+        <div className="flex flex-row">
+          {user ? (
+            <Link to="/profile">
+              <div
+                className="flex flex-row-reverse"
+                data-tooltip-id="avatar"
+                data-tooltip-content="Accedez à votre profil"
+              >
+                <img
+                  src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
+                  alt="Discord Profile"
+                  className="rounded-full w-10 h-10"
+                />
+                <span className="mt-2 mr-2">{user.global_name}</span>
+              </div>
+              <Tooltip id="avatar" place="bottom" />
+            </Link>
+          ) : (
+            <button
+              onClick={handleLogin}
+              className="bg-gray-200 px-7 rounded-xl drop-shadow-lg ml-20 transition-all transform hover:bg-gray-300"
+            >
+              Se Connecter
+            </button>
+          )}
+        </div>
+      </header>
+      <div className="m-8 w-10/12">
+        <div className="flex flex-row">
+          <div className="w-1/5">
+            <p className="text-lg">Filtres de recherche :</p>
+            <br />
+            <div className="flex flex-col gap-8">
+              <div>
+                <p>Jeu</p>
+                <select
+                  name="jeu"
+                  className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300"
+                >
+                  <option value="ultimate">Smash Ultimate</option>
+                  <option value="hdr">Smash HDR</option>
+                  <option value="sm4sh">Sm4sh</option>
+                  <option value="brawl">Smash Brawl</option>
+                  <option value="melee">Smash Melee</option>
+                </select>
+              </div>
+              <div>
+                <div className="flex flex-row space-x-1">
+                  <p>Distance :</p>
+                  <output>{distance} km</output>
+                </div>
+                <div>
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value={distance}
+                    onChange={handleDistanceChange}
+                    className="w-full"
+                  />
+                  <div className="flex flex-row justify-between">
+                    <p>0 km</p>
+                    <p>100 km</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-row justify-between">
+                <p>Afficher les sessions pleines</p>
+                <input type="checkbox"></input>
+              </div>
+              <div>
+                <p>Horaire</p>
+                <input
+                  type="datetime-local"
+                  className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300"
+                ></input>
+              </div>
+              <div>
+                <p>Niveau</p>
+                <select
+                  name="niveau"
+                  className="bg-gray-200 rounded-lg p-3 w-full transition-all transform hover:bg-gray-300"
+                >
+                  <option value="debutant">Débutant</option>
+                  <option value="intermediaire">Intermédiaire</option>
+                  <option value="expert">Expert</option>
+                </select>
+              </div>
+              <div className="flex flex-col text-lg justify-center align-bottom mt-24 h-auto">
+                <p>Credits</p>
+                <p>Conditions d'utilisation</p>
+                <p>Contacts</p>
+              </div>
+            </div>
+          </div>
+          {/* Separation */}
+          <div className="flex flex-col ml-10 w-10/12">
+            <div className="flex flex-col w-full">
+              <div className="flex flex-row justify-between w-full">
+                <p className="text-lg mr-10">Sessions proches de chez moi</p>
+                <button className="bg-gray-200 px-7 rounded-xl drop-shadow-lg transition-all transform hover:bg-gray-300">
+                  <Link to="/create">Créer une session</Link>
+                </button>
+              </div>
+              <div className="mt-14 h-screen">
+                <p>aa</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/Profile.js
+++ src/Profile.js
@@ -1,70 +1,71 @@
 import React, { useState } from 'react';
-import Tooltip from 'react-tooltip-lite';
-import { Link, useNavigate } from 'react-router-dom';
-import { useTheme } from './common/ThemeContext';
-import sun from './Assets/sun.svg';
-import moon from './Assets/moon.svg';
+import { Tooltip } from "react-tooltip";
+import { Link, useNavigate } from "react-router-dom";
+import { useTheme } from "./common/ThemeContext";
+import sun from "./Assets/sun.svg";
+import moon from "./Assets/moon.svg";
 
 function Profile({ user }) {
   const { theme, toggleTheme } = useTheme();
-  const themeIcon = theme === 'light' ? sun : moon;
-  const [tooltipText, setTooltipText] = useState('Cliquez pour copier');
+  const themeIcon = theme === "light" ? sun : moon;
+  const [tooltipText, setTooltipText] = useState("Cliquez pour copier");
   const navigate = useNavigate();
   const handleClick = () => {
     navigator.clipboard.writeText(user.username).then(() => {
       setTooltipText(`Identifiant "${user.username}" copié`);
       setTimeout(() => {
-        setTooltipText('Cliquez pour copier le pseudo discord');
+        setTooltipText("Cliquez pour copier le pseudo discord");
       }, 2000);
     });
   };
 
   const Logout = () => {
-    localStorage.removeItem('user');
-    navigate('/');
+    localStorage.removeItem("user");
+    navigate("/");
     window.location.reload();
-  }
+  };
 
   return (
     <div>
-      <header className='flex flex-row justify-evenly space-x-2'>
+      <header className="flex flex-row justify-evenly space-x-2">
         <p>NaetorUSmash</p>
         <div>
           <Link to="/">Accueil</Link>
         </div>
         {/* Icône de changement de thème */}
-        <button onClick={toggleTheme} className='flex items-center'>
-          <img src={themeIcon} alt='Theme icon' className='size-8' />
+        <button onClick={toggleTheme} className="flex items-center">
+          <img src={themeIcon} alt="Theme icon" className="size-8" />
         </button>
         <button onClick={Logout}>Se déconnecter</button>
       </header>
       {user && (
-        <div className='flex flex-row justify-center content-center w-auto'>
-          <div className='flex flex-col'>
-            <div className='flex justify-center'>
+        <div className="flex flex-row justify-center content-center w-auto">
+          <div className="flex flex-col">
+            <div className="flex justify-center">
               <img
                 src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
-                alt='Discord Profile'
-                className='flex justify-center rounded-full size-72 border-2 border-black'
+                alt="Discord Profile"
+                className="flex justify-center rounded-full size-72 border-2 border-black"
               />
             </div>
-            <div className='text-center space-y-1 mt-2'>
+            <div className="text-center space-y-1 mt-2">
               <p>Pseudo : {user.global_name}</p>
-              <Tooltip content={tooltipText} direction="left" tagName="span" className="tooltip">
-                <span
-                  onClick={handleClick}
-                  style={{ cursor: 'pointer', display: 'inline-block' }}
-                >
-                  Discord Id : {user.id}
-                </span>
-              </Tooltip>
+              <span
+                onClick={handleClick}
+                style={{ cursor: "pointer", display: "inline-block" }}
+                data-tooltip-id="discordId"
+                data-tooltip-content={tooltipText}
+              >
+                Discord Id : {user.id}
+              </span>
+              <Tooltip id="discordId" place="bottom" />
               <p>Sessions Organisées : </p>
               <p>Participation en Session : </p>
               <div>
-                <p>Les Notes de la communauté seront ajoutées dans le futur !</p>
-                <div id="Stars">
-                  {/* Placeholder pour les notes stars */}
-                </div>
+                <p>
+                  Les Notes de la communauté seront ajoutées dans le futur !
+                </p>
+                <div id="Stars">{/* Placeholder pour les notes stars */}</div>
               </div>
             </div>
           </div>
