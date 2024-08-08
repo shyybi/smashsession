ae90b19de0b3b14336005fbb7212c80671906f09
IgenFlo <floehr31@gmail.com>
2024-08-08 03:42:04 +0200

    fix: re attach jwt token to requests on app startup to keep user connected

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Profile from "./Profile";
-import Home from "./Home";
-import Create from "./Create";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
 import { useTheme } from "./common/ThemeContext";
 import { useMe } from "./api/hooks/users.hooks";
+import { attachToken } from "./api/privateAxiosInstance";
 
 function App() {
   const { theme } = useTheme();
   const { me, isLoading } = useMe();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      attachToken(token);
+    }
+  }, []);
 
   useEffect(() => {
     const htmlElement = document.documentElement;
@@ -21,9 +29,16 @@ function App() {
 
   return (
     <Router>
-      <div className={`min-h-screen ${theme === "dark" ? "bg-[#1E1E1E] text-white" : "bg-white text-black"}`}>
+      <div
+        className={`min-h-screen ${
+          theme === "dark" ? "bg-[#1E1E1E] text-white" : "bg-white text-black"
+        }`}
+      >
         <Routes>
-          <Route path="*" element={<Home user={me} isLoadingUser={isLoading} />} />
+          <Route
+            path="*"
+            element={<Home user={me} isLoadingUser={isLoading} />}
+          />
           <Route path="/profile" element={<Profile user={me} />} />
           <Route path="/create" element={<Create user={me} />} />
         </Routes>
